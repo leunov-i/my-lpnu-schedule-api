7d0c1f8242e89df735d884a46727a398fc3b05ed
Illia Leunov <illia.leunov@gmail.com>
2025-04-17 01:50:11 +0300

    update2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from .routes import api
+from app.routes import schedule_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api)
+    app.register_blueprint(schedule_blueprint)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, jsonify
-from .parser import get_schedule_data
-from urllib.parse import quote
+from app.parser import get_schedule_data
 
-api = Blueprint("api", __name__)
+schedule_blueprint = Blueprint("schedule", __name__)
 
-@api.route("/api/schedule/<group_name>")
-def schedule(group_name):
+@schedule_blueprint.route("/<group>", methods=["GET"])
+def get_schedule(group):
     try:
-        group_encoded = quote(group_name)
-        data = get_schedule_data(group_encoded)
+        data = get_schedule_data(group)
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
